bca0ee436505a3a94626d09004dd8e7c673d1cfe
Sam Morley-Short <sam.morleyshort@gmail.com>
2017-10-23 17:37:40 +0100

    Added start grid and intensity map function

--- contour_map.py
+++ contour_map.py
@@ -44,7 +44,7 @@ def get_pixel_neighbourhood(pixel, frame_size, binary_map):
     return new_coords
 
 
-def build_contour_map(pixdata, levels=1):
+def build_contour_map(pixdata, levels=1, grid_res=10):
     """
     Builds contour map with given number of levels from input pixel data
     """
@@ -55,45 +55,53 @@ def build_contour_map(pixdata, levels=1):
     std = find_std(pixdata)
     start_pixel = get_centre_pixel(pixdata)
     frame_size = len(pixdata)
+    grid_lines = range(frame_size)[::grid_res]
+    pixel_grid = list(it.product(grid_lines, grid_lines))
     # For each contour level builds bitmap
     binary_maps = []
     for i, contour in enumerate(contours):
         # Provides a blank bitmap for each run
         binary_map = np.full_like(pixdata, None)
         # If first pixel not bright enough, sets bitmap to blank canvas
-        if pixdata[start_pixel] < (contour * std):
-            print "Initial pixel not hot enough (< %f)" % (contour)
-            binary_maps += [np.full_like(pixdata, 0)]
-            break
         increment = contour - contours[i-1] if i > 0 else contour
-        binary_map[start_pixel] = increment
-        # Gets initial set of starter pixels
-        active_pix = \
-            get_pixel_neighbourhood(start_pixel, frame_size, binary_map)
-        # While active pixels exist, keep searching
-        while active_pix:
-            # Tracks pixels that are found above contour this round
-            gen_pix = []
-            # If pixel below contour, set to 0, else 1 and add to gen pixels
-            for pixel in active_pix:
-                if pixdata[pixel] > (contour * std):
-                    binary_map[pixel] = increment
-                    gen_pix += [pixel]
-                else:
-                    binary_map[pixel] = 0
-            # Gets next set of active pixels from gen pixels
+        for start_pixel in pixel_grid:
+            if pixdata[start_pixel] < (contour * std):
+                continue
+            binary_map[start_pixel] = increment
+            # Gets initial set of starter pixels
             active_pix = \
-                set(flatten([get_pixel_neighbourhood(gen_pixel, frame_size,
-                                                     binary_map)
-                             for gen_pixel in gen_pix]))
+                get_pixel_neighbourhood(start_pixel, frame_size, binary_map)
+            # While active pixels exist, keep searching
+            while active_pix:
+                # Tracks pixels that are found above contour this round
+                gen_pix = []
+                # If pixel below contour, set to 0, else 1 and add to gen
+                # pixels
+                for pixel in active_pix:
+                    if pixdata[pixel] > (contour * std):
+                        binary_map[pixel] = increment
+                        gen_pix += [pixel]
+                    else:
+                        binary_map[pixel] = 0
+                        if pixel in pixel_grid:
+                            pixel_grid.remove(pixel)
+                # Gets next set of active pixels from gen pixels
+                active_pix = \
+                    set(flatten([get_pixel_neighbourhood(gen_pixel, frame_size,
+                                                         binary_map)
+                                 for gen_pixel in gen_pix]))
         # Saves bitmap
         binary_maps += [np.nan_to_num(binary_map)]
+        if len(np.unique(binary_maps[-1])) == 1:
+            print "No region of > %d * std found" % contour
+            break
     # Adds all the binary maps together to produce final map
     final_map = reduce(np.add, binary_maps)
     return final_map
 
 
-def batch_apply_bitmap(main_dir, fits_subdir, img_subdir, bm_subdir, levels):
+def batch_apply_bitmap(main_dir, fits_subdir, img_subdir, bm_subdir, levels,
+                       intesity_map=lambda x: x):
     """
     Applies bitmap to multiple fits files in directory and outputs images to
     another subdirectory. Writes pngs with same name as fits file
@@ -115,6 +123,7 @@ def batch_apply_bitmap(main_dir, fits_subdir, img_subdir, bm_subdir, levels):
                 pixdata = f[0].data
                 # Builds contour map
                 final_map = build_contour_map(pixdata, levels)
+                final_map = intesity_map(final_map)
                 final_map = final_map.astype(int)
                 # print np.unique(final_map)
                 # Saves bitmap
@@ -136,6 +145,7 @@ if __name__ == '__main__':
     img_subdir = 'imgs'
     bm_subdir = 'bitmaps'
 
-    levels = [1, 5, 10]
+    levels = [1, 5, 10, 50, 100, 200, 500, 1000, 1500, 2000]
 
-    batch_apply_bitmap(main_dir, fits_subdir, img_subdir, bm_subdir, levels)
+    batch_apply_bitmap(main_dir, fits_subdir, img_subdir, bm_subdir, levels,
+                       intesity_map=lambda x: x ** 0.5)
